3b177d8c99368fd580e59c572d39bc0b1fe84cc8
2B-0128 <arlenys@2brains.cl>
2021-10-21 08:50:06 -0300

    add route tet

--- src/application/controllers/vtex.controller.ts
+++ src/application/controllers/vtex.controller.ts
@@ -1,130 +1,181 @@
-import { Body, Controller, Get, Logger, Param, Post, Res } from "@nestjs/common";
-import { Response } from 'express'
-import { PaymentMethodsDto } from "../dto/payment-methods.dto";
-import { HeadersDTO } from "../dto/headers.dto";
-import { RequestHeader } from "../dto/request-header.decorator";
-import { PaymentRequestDTO } from "../dto/payment-request.dto";
-import { VtexService } from "../../domain/services/vtex.service";
-import { PaymentResponseDto } from "../dto/payment-response.dto";
-import { CancellationRequestDTO, CancellationResponseDTO } from "../dto/cancellation.dto";
-import { SettlementsRequestDTO, SettlementsResponseDTO } from "../dto/settlements.dto";
-import { RefundRequestDTO, RefundResponseDTO } from "../dto/refund.dto";
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { PaymentMethodsDto } from '../dto/payment-methods.dto';
+import { HeadersDTO } from '../dto/headers.dto';
+import { RequestHeader } from '../dto/request-header.decorator';
+import { PaymentRequestDTO } from '../dto/payment-request.dto';
+import { VtexService } from '../../domain/services/vtex.service';
+import { PaymentResponseDto } from '../dto/payment-response.dto';
+import {
+  CancellationRequestDTO,
+  CancellationResponseDTO,
+} from '../dto/cancellation.dto';
+import {
+  SettlementsRequestDTO,
+  SettlementsResponseDTO,
+} from '../dto/settlements.dto';
+import { RefundRequestDTO, RefundResponseDTO } from '../dto/refund.dto';
 
 @Controller('api/vtex')
 export class VtexController {
-    constructor(private vtexService: VtexService,
-                private readonly logger: Logger,) {
-    }
+  constructor(
+    private vtexService: VtexService,
+    private readonly logger: Logger,
+  ) {}
 
-    /**
-     * @api {get} /payment-methods Request information on payment methods
-     * @apiName Payment methods
-     *
-     * @apiSuccess {Array} paymentMethods Payment methods VTEX.
-     */
-    @Get('/payment-methods')
-    async paymentMethods(@RequestHeader(HeadersDTO) headers: any): Promise<PaymentMethodsDto> {
-        return {
-            paymentMethods: ['Promissories']
-        };
-    }
+  /**
+   * @api {get} /payment-methods Request information on payment methods
+   * @apiName Payment methods
+   *
+   * @apiSuccess {Array} paymentMethods Payment methods VTEX.
+   */
+  @Get('/payment-methods')
+  async paymentMethods(
+    @RequestHeader(HeadersDTO) headers: any,
+  ): Promise<PaymentMethodsDto> {
+    return {
+      paymentMethods: ['Promissories'],
+    };
+  }
 
-    /**
-     * @api {get} /payment-methods Request information on payment methods
-     * @apiName Payment methods
-     *
-     * @apiSuccess {Array} paymentMethods Payment methods VTEX.
-     */
-    @Get('public/payment-method')
-    async paymentMethodsPublic(): Promise<PaymentMethodsDto> {
-        return {
-            paymentMethods: ['Promissories']
-        };
-    }
+  /**
+   * @api {get} /payment-methods Request information on payment methods
+   * @apiName Payment methods
+   *
+   * @apiSuccess {Array} paymentMethods Payment methods VTEX.
+   */
+  @Get('public/payment-method')
+  async paymentMethodsPublic(): Promise<PaymentMethodsDto> {
+    return {
+      paymentMethods: ['Promissories'],
+    };
+  }
 
-    /**
-     * @api {post} /payments Receive information about the transaction
-     * @apiName Payments
-     *
-     * @apiDescription
-     */
-    @Post('/payments')
-    async payments(@RequestHeader(HeadersDTO) headers: any, @Body() paymentRequest: PaymentRequestDTO, @Res() response: Response): Promise<PaymentResponseDto> {
-        //throw new BadRequestException({message: ['asd','dsa']})
-        const result: PaymentResponseDto = await this.vtexService.payment(paymentRequest);
-        response.status(result.tid ? 200 : 500).send(result).end();
-        return;
-    }
+  /**
+   * @api {post} /payments Receive information about the transaction
+   * @apiName Payments
+   *
+   * @apiDescription
+   */
+  @Post('/payments')
+  async payments(
+    @RequestHeader(HeadersDTO) headers: any,
+    @Body() paymentRequest: PaymentRequestDTO,
+    @Res() response: Response,
+  ): Promise<PaymentResponseDto> {
+    //throw new BadRequestException({message: ['asd','dsa']})
+    const result: PaymentResponseDto = await this.vtexService.payment(
+      paymentRequest,
+    );
+    response
+      .status(result.tid ? 200 : 500)
+      .send(result)
+      .end();
+    return;
+  }
 
-    /**
-     * @api {post} /payments/:paymentId/cancellations Cancels a payment that was not yet approved or captured
-     * @apiName Cancel Payment
-     *
-     * @apiParam {String} id payment unique ID.
-     *
-     * @apiSuccess {String} paymentId      VTEX payment ID from this payment.
-     * @apiSuccess {String} cancellationId Provider's cancellation identifier.
-     * @apiSuccess {String} code           Provider's operation/error code to be logged.
-     * @apiSuccess {String} message        Provider's operation/error message to be logged.
-     * @apiSuccess {String} requestId      The unique identifier for this request to ensure its idempotency.
-     */
-    @Post('/payments/:paymentId/cancellations')
-    async cancellation(@RequestHeader(HeadersDTO) headers: any,
-                       @Param('paymentId') paymentId: string,
-                       @Body() cancellationRequest: CancellationRequestDTO,
-                       @Res() response: Response): Promise<CancellationResponseDTO> {
-        const result: CancellationResponseDTO = await this.vtexService.cancellation(cancellationRequest);
-        response.status(result.cancellationId ? 200 : 500).send(result).end();
-        return;
+  /**
+   * @api {post} /payments/:paymentId/cancellations Cancels a payment that was not yet approved or captured
+   * @apiName Cancel Payment
+   *
+   * @apiParam {String} id payment unique ID.
+   *
+   * @apiSuccess {String} paymentId      VTEX payment ID from this payment.
+   * @apiSuccess {String} cancellationId Provider's cancellation identifier.
+   * @apiSuccess {String} code           Provider's operation/error code to be logged.
+   * @apiSuccess {String} message        Provider's operation/error message to be logged.
+   * @apiSuccess {String} requestId      The unique identifier for this request to ensure its idempotency.
+   */
+  @Post('/payments/:paymentId/cancellations')
+  async cancellation(
+    @RequestHeader(HeadersDTO) headers: any,
+    @Param('paymentId') paymentId: string,
+    @Body() cancellationRequest: CancellationRequestDTO,
+    @Res() response: Response,
+  ): Promise<CancellationResponseDTO> {
+    const result: CancellationResponseDTO = await this.vtexService.cancellation(
+      cancellationRequest,
+    );
+    response
+      .status(result.cancellationId ? 200 : 500)
+      .send(result)
+      .end();
+    return;
+  }
 
-    }
+  /**
+   * @api {post} /payments/:paymentId/settlements Captures (settle) a payment that was previously approved
+   * @apiName Capture Payment
+   *
+   * @apiParam {String} id payment unique ID.
+   *
+   * @apiSuccess {String} paymentId VTEX payment ID from this payment.
+   * @apiSuccess {String} settleId  Provider's settlement identifier.
+   * @apiSuccess {String} value     The amount that was settled/captured.
+   * @apiSuccess {String} code      Provider's operation/error code to be logged.
+   * @apiSuccess {String} message   Provider's operation/error message to be logged.
+   * @apiSuccess {String} requestId The unique identifier for this request to ensure its idempotency.
+   */
+  @Post('/payments/:paymentId/settlements')
+  async settlements(
+    @RequestHeader(HeadersDTO) headers: any,
+    @Param('paymentId') paymentId: string,
+    @Body() settlementsRequest: SettlementsRequestDTO,
+    @Res() response: Response,
+  ): Promise<SettlementsResponseDTO> {
+    const result: SettlementsResponseDTO = await this.vtexService.settlements(
+      settlementsRequest,
+    );
 
-    /**
-     * @api {post} /payments/:paymentId/settlements Captures (settle) a payment that was previously approved
-     * @apiName Capture Payment
-     *
-     * @apiParam {String} id payment unique ID.
-     *
-     * @apiSuccess {String} paymentId VTEX payment ID from this payment.
-     * @apiSuccess {String} settleId  Provider's settlement identifier.
-     * @apiSuccess {String} value     The amount that was settled/captured.
-     * @apiSuccess {String} code      Provider's operation/error code to be logged.
-     * @apiSuccess {String} message   Provider's operation/error message to be logged.
-     * @apiSuccess {String} requestId The unique identifier for this request to ensure its idempotency.
-     */
-    @Post('/payments/:paymentId/settlements')
-    async settlements(@RequestHeader(HeadersDTO) headers: any,
-                      @Param('paymentId') paymentId: string,
-                      @Body() settlementsRequest: SettlementsRequestDTO,
-                      @Res() response: Response): Promise<SettlementsResponseDTO> {
-        const result: SettlementsResponseDTO = await this.vtexService.settlements(settlementsRequest);
+    response
+      .status(result.settleId ? 200 : 500)
+      .send(result)
+      .end();
+    return;
+  }
 
-        response.status(result.settleId ? 200 : 500).send(result).end();
-        return;
-    }
+  /**
+   * @api {post} /payments/:paymentId/refunds Refunds a payment that was previously captured (settled)
+   * @apiName Capture Payment
+   *
+   * @apiParam {String} id payment unique ID.
+   *
+   * @apiSuccess {String} paymentId VTEX payment ID from this payment.
+   * @apiSuccess {String} refundId  Provider's refund identifier.
+   * @apiSuccess {String} value     The amount that was settled/captured.
+   * @apiSuccess {String} code      Provider's operation/error code to be logged.
+   * @apiSuccess {String} message   Provider's operation/error message to be logged.
+   * @apiSuccess {String} requestId The unique identifier for this request to ensure its idempotency.
+   */
+  @Post('/payments/:paymentId/refunds')
+  async refund(
+    @RequestHeader(HeadersDTO) headers: any,
+    @Param('paymentId') paymentId: string,
+    @Body() refundRequest: RefundRequestDTO,
+    @Res() response: Response,
+  ): Promise<SettlementsResponseDTO> {
+    const result: RefundResponseDTO = await this.vtexService.refund(
+      refundRequest,
+    );
 
-    /**
-     * @api {post} /payments/:paymentId/refunds Refunds a payment that was previously captured (settled)
-     * @apiName Capture Payment
-     *
-     * @apiParam {String} id payment unique ID.
-     *
-     * @apiSuccess {String} paymentId VTEX payment ID from this payment.
-     * @apiSuccess {String} refundId  Provider's refund identifier.
-     * @apiSuccess {String} value     The amount that was settled/captured.
-     * @apiSuccess {String} code      Provider's operation/error code to be logged.
-     * @apiSuccess {String} message   Provider's operation/error message to be logged.
-     * @apiSuccess {String} requestId The unique identifier for this request to ensure its idempotency.
-     */
-    @Post('/payments/:paymentId/refunds')
-    async refund(@RequestHeader(HeadersDTO) headers: any,
-                 @Param('paymentId') paymentId: string,
-                 @Body() refundRequest: RefundRequestDTO,
-                 @Res() response: Response): Promise<SettlementsResponseDTO> {
-        const result: RefundResponseDTO = await this.vtexService.refund(refundRequest);
+    response
+      .status(result.refundId ? 200 : 500)
+      .send(result)
+      .end();
 
-        response.status(result.refundId ? 200 : 500).send(result).end();
+    return;
+  }
 
-        return;
-    }
+  @Get('/')
+  test() {
+    return 'hola desde provider';
+  }
 }
